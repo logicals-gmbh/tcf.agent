a5f02b922abe7da4cad1a3d88568391078d34346
Martin Ebner <martin.ebner@logicals.com>
2018-05-07 14:58:24 +0200

    LCOIII-25129: NDEBUG disables assert

--- rtos-32/assert.h
+++ rtos-32/assert.h
@@ -4,14 +4,19 @@
 #if TARGET_RTOS32
  /* The windows assert function opens a message box. 
   * On RTOS-32 the function is overloaded with a console assert: */
- #define assert(cond) \
- { \
-   if((cond) == 0) \
-   { \
-     printf("FATAL: assert failed: %s:%d\n",__FILE__,__LINE__ ); \
-     exit(0); \
-   } \
- }
-#endif
+#ifdef NDEBUG
+#define assert(condition) ((void)0)
+#else
+#define assert(cond) \
+{ \
+  if((cond) == 0) \
+  { \
+    printf("FATAL: assert failed: %s:%d\n",__FILE__,__LINE__ ); \
+    exit(0); \
+  } \
+}
+#endif /* NDEBUG */
+
+#endif /* TARGET_RTOS32 */
 
 #endif /* RTOS32_ASSERT_H */
